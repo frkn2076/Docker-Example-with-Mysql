e62f4d310f63226dd69a2e5a7fe7c15d596fdd6d
frkn2076 <ozturkfurkan1994@hotmail.com>
2020-03-15 10:33:52 +0300

    Docker

---
+++ DockerExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DockerExample {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                // Connect to SQL
+                string connectionStr = @"server=localhost,3306;userid=root;password=toortoor;database=testdb";
+                String query = "select * from MyTable";
+                String output = string.Empty;
+                using(MySqlConnection connection = new MySqlConnection(connectionStr)) {
+                    
+                    MySqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = query;
+                    connection.Open();
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while(reader.Read()) {
+                        output = string.Concat(output, reader.GetString("Id"), " - ", reader.GetString("Name"), " ", reader.GetString("Surname"),"\n");
+                    }
+
+                    Console.Write(output);
+
+                    connection.Close();
+                }
+            }
+            catch(SqlException e) {
+                Console.WriteLine(e.ToString());
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
